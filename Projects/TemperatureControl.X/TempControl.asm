88360269715ed70b40b40a3e186cb8b84d720e50
Calvin Pereira <78508059+CalvinJDP@users.noreply.github.com>
2024-03-05 00:06:39 -0800

    Update TempControl.asm

--- Projects/TemperatureControl.X/TempControl.asm
+++ Projects/TemperatureControl.X/TempControl.asm
@@ -18,56 +18,80 @@
 //  	
 //-----------------------------
 
-    
+#include ".\newAsmTemplate.inc"
+#include <xc.inc>
     
 ;----------------
 ; PROGRAM INPUTS
 ;----------------
-#define	    measuredTempInput 	45d	; Input for measured from sensor
-#define	    refTempInput	25d	; Input from user
+measuredTempInput   equ 	45		; Input for measured from sensor
+refTempInput	    equ		25		; Input from user
 
 ;----------------
 ; REGISTERS
 ;----------------
-#define	    measuredTempREG 	0x20	; Setting reg 20 to store measured temp
-#define	    refTempREG 		0x21	; Setting reg 21 to store reference temp
-
+measuredTempREG	    equ 	0x20		; Setting reg 20 to store measured temp
+refTempREG	    equ		0x21		; Setting reg 21 to store reference temp
+	    
 
 
 ;----------------
 ; PROGRAM OUTPUTS
 ;----------------
-#define	    DCrefTempA		0x60	; Used to store decimal value of Refernece Temp
-#define	    DCrefTempB		0x61	; Used to store decimal value of Refernece Temp
-#define	    DCrefTempC		0x62	; Used to store decimal value of Refernece Temp
+DCrefTempA	    equ		0x60		; Used to store decimal value of Refernece Temp
+DCrefTempB	    equ		0x61		; Used to store decimal value of Refernece Temp
+DCrefTempC	    equ		0x62		; Used to store decimal value of Refernece Temp
     
-#define	    DCmesTempA		0x70	; Used to store decimal value of Measured Temp
-#define	    DCmesTempB		0x71	; Used to store decimal value of Measured Temp
-#define	    DCmesTempC		0x72	; Used to store decimal value of Measured Temp
+DCmesTempA	    equ		0x70		; Used to store decimal value of Measured Temp
+DCmesTempB	    equ		0x71		; Used to store decimal value of Measured Temp
+DCmesTempC	    equ		0x72		; Used to store decimal value of Measured Temp
     
-#define	    HEATER    PORTD,2		; Sets PORTD 2 to the heater LED
-#define	    COOLER    PORTD,1		; Sets PORTD 1 to the coller LED
+#define		    HEATER      PORTD,2		; Sets PORTD 2 to the heater LED
+#define		    COOLER	PORTD,1		; Sets PORTD 1 to the coller LED
+	   
+	   
+;----------------
+; Main Program
+;----------------
+	    org 0
+
+START:
+    
+    MOVLW   0b11111100
+    MOVWF   TRISD, 0
+    MOVFF   measuredTempInput, measuredTempREG
+    MOVFF   refTempInput,refTempREG
+    MOVFF   measuredTempREG, WREG		; Move contents of measuredTempREG to WREG
+
+    CPFSEQ  refTempREG, 0
+    GOTO    LED_OFF
+    
+    CPFSGT refTempREG, 0
+    GOTO   LED_HOT
+    
+    CPFSLT refTempREG, 0
+    GOTO   LED_COOL
 
     
-    If measuredTemp=refTemp then
-	set contReg = 0
-	GOTO LED_OFF
-
-    If measuredTemp>refTemp then
-	set contReg = 2
-	GOTO LED_HOT
-	
-    else
-	set contReg = 1
-	GOTO LED_COOL
-	
 LED_HOT:
-	
+    ; Code for when the temperature is hot
+    ; Example: Turn on the hot air blower LED
+    BSF     HEATER, 0 ; Set PORTD,2 (Heater LED)
+    BCF     COOLER, 1 ; Clear PORTD,1 (Cooling LED)
+    GOTO    START ; Jump to the end of LED control
+    
+LED_COOL:
+    ; Code for when the temperature is cool
+    ; Example: Turn on the cooling fan LED
+    BSF     COOLER, 0 ; Set PORTD,1 (Cooling LED)
+    BCF     HEATER, 1 ; Clear PORTD,2 (Heater LED)
+    GOTO    START ; Jump to the end of LED control
+    
+LED_OFF:
+    ; Code for when the temperature is at the desired level
+    ; Example: Turn off both LEDs
+    BCF     COOLER, 0 ; Clear PORTD,1 (Cooling LED)
+    BCF     HEATER, 0 ; Clear PORTD,2 (Heater LED)
+    GOTO    START
 
-	
-LED_COOl:
-	
-		
-LED_OFF: 
-	
 	

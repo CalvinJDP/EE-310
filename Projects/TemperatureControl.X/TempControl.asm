c41859118221c808b06541dc9bb1c62a46e087ec
Calvin Pereira <78508059+CalvinJDP@users.noreply.github.com>
2024-03-06 22:41:51 -0800

    Update TempControl.asm
    Fully working code

--- Projects/TemperatureControl.X/TempControl.asm
+++ Projects/TemperatureControl.X/TempControl.asm
@@ -25,13 +25,16 @@
 ; PROGRAM INPUTS
 ;----------------
 measuredTempInput   equ 	0		; Input for measured from sensor
-refTempInput	    equ		10		; Input from user
-
+refTempInput	    equ		15		; Input from user
+NUME		    equ		0x22
+QU		    equ		0x23
+MYDEN		    equ		10
 ;----------------
 ; REGISTERS
 ;----------------
-measuredTempREG	    equ 	20h		; Setting reg 20 to store measured temp
-refTempREG	    equ		21h		; Setting reg 21 to store reference temp	    
+measuredTempREG	    equ 	21h		; Setting reg 20 to store measured temp
+refTempREG	    equ		20h		; Setting reg 21 to store reference temp
+contReg		    equ		22h
 
 
 ;----------------
@@ -45,8 +48,8 @@ DCmesTempA	    equ		0x70		; Used to store decimal value of Measured Temp
 DCmesTempB	    equ		0x71		; Used to store decimal value of Measured Temp
 DCmesTempC	    equ		0x72		; Used to store decimal value of Measured Temp
     
-#define		    HEATER      PORTD,1		; Sets PORTD 2 to the heater LED
-#define		    COOLER	PORTD,2		; Sets PORTD 1 to the coller LED
+#define		    HEATER      PORTD,2		; Sets PORTD 2 to the heater LED
+#define		    COOLER	PORTD,1		; Sets PORTD 1 to the coller LED
 	   
 	   
 ;----------------
@@ -62,45 +65,101 @@ DCmesTempC	    equ		0x72		; Used to store decimal value of Measured Temp
 
 START:
     
-    MOVLW   0b11111001
-    MOVWF   TRISD, 0
-    MOVLW   measuredTempInput
-    MOVWF   measuredTempREG
-    MOVLW   refTempInput
-    MOVWF   refTempREG
+	MOVLW   0b11111001
+	MOVWF   TRISD, 0
+	MOVLW   measuredTempInput
+    	MOVWF   measuredTempREG
+	MOVLW   refTempInput
+	MOVWF   refTempREG
     
-    GOTO    TempCheck
+	GOTO    AH_D
     
 TempCheck:
-    MOVFF   measuredTempREG, WREG
+	MOVFF   measuredTempREG, WREG
     
-    BTFSC   measuredTempREG, 7, 0
-    GOTO    LED_HOT
+	BTFSC   measuredTempREG, 7, 0
+	GOTO    LED_HOT
     
-    CPFSEQ  refTempREG, 0
-    GOTO    NotEqual
-    GOTO    LED_OFF
+	CPFSEQ  refTempREG, 0
+	GOTO    NotEqual
+	GOTO    LED_OFF
 
     
 NotEqual:
-    CPFSGT refTempREG,0
-    GOTO   LED_COOL
-    GOTO   LED_HOT
+	CPFSGT refTempREG,0
+	GOTO   LED_COOL
+	GOTO   LED_HOT
     
     
     
 LED_HOT:
-    BSF     HEATER
-    GOTO    TempCheck
+	BSF     HEATER
+	MOVLW	2h
+	MOVWF	contReg
+	GOTO    TempCheck
     
 LED_COOL:
-    BSF	    COOLER
-    GOTO    TempCheck
+	BSF	    COOLER
+	MOVLW	1h
+	MOVWF	contReg
+	GOTO    TempCheck
     
 LED_OFF:
-    BCF     COOLER, 0 ; Clear PORTD,1 (Cooling LED)
-    BCF     HEATER, 0 ; Clear PORTD,2 (Heater LED)
-    GOTO    TempCheck
+	BCF     COOLER, 0 ; Clear PORTD,1 (Cooling LED)
+	BCF     HEATER, 0 ; Clear PORTD,2 (Heater LED)
+	MOVLW	0h
+	MOVWF	contReg
+	GOTO    TempCheck
+    
+AH_D:
+	MOVFF   refTempREG, WREG
+	MOVWF   NUME
+	MOVLW   MYDEN
+	CLRF    QU
+	GOTO AD_1
 
+AD_1:	INCF    QU, F
+	SUBWF   NUME
+	BC	    AD_1
+	ADDWF   NUME
+	DECF    QU, F
+	MOVFF   NUME, DCrefTempC
+	MOVFF   QU, NUME
+	CLRF    QU
+	GOTO AD_2
+AD_2:	INCF    QU, F
+	SUBWF   NUME
+	BC	    AD_2
+	ADDWF   NUME
+	DECF    QU, F
+	MOVFF   NUME, DCrefTempB
+	MOVFF   QU, DCrefTempA
+	GOTO BH_D
+	
+BH_D:
+	MOVFF   measuredTempREG, WREG
+	MOVWF   NUME
+	MOVLW   MYDEN
+	CLRF    QU
+	GOTO BD_1
+
+BD_1:	INCF    QU, F
+	SUBWF   NUME
+	BC	    BD_1
+	ADDWF   NUME
+	DECF    QU, F
+	MOVFF   NUME, DCmesTempC
+	MOVFF   QU, NUME
+	CLRF    QU
+	GOTO BD_2
+BD_2:	INCF    QU, F
+	SUBWF   NUME
+	BC	    BD_2
+	ADDWF   NUME
+	DECF    QU, F
+	MOVFF   NUME, DCmesTempB
+	MOVFF   QU, DCmesTempA
+	GOTO TempCheck
+    
 
 	
